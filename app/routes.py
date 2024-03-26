d27108c4bd6e3ae9fdce17bef55e418968f9440f
MaartenTC <MaartenTC@outlook.com>
2024-03-26 15:52:32 +0100

    controller

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["POST"])
 def controller():
-    data = request.get_json()
-    returnval = services.data_sort(data)
-    return jsonify(returnval), 200
-
-
+    try:
+        data = request.get_json()
+        returnval = services.data_sort(data)
+        return jsonify(returnval), 200
+    except Exception as e:
+        return jsonify(message=str(e)), 500
+    
+    
 if __name__ == "__main__":
     app.run(debug= True)
