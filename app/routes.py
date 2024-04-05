de61f1897c45cb079ca2eb68f1fa0bef2c9cdeff
TylerRijk <tyler.derijk@student.hu.nl>
2024-04-05 12:06:06 +0200

    Added amount vehicles option with mock request

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, jsonify
-from app.services import *
+from services import *
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/report', methods = ["POST"])
 def controller():
     try:
         data = request.get_json()
-        returnval = data_sort(data)
-        return jsonify(returnval), 200
+        print(data)
+        result = data_sort(data)
+        return result
     except Exception as e:
         return jsonify(message=str(e)), 500
     
